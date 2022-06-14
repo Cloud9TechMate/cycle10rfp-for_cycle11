b941bbc263b19ef7953f4d5442add97c7379a219
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-13 23:41:33 -0500

    Finished
    There is no input though. just a grab from the backend and post on the frontend. Have to css it a bit.

---
+++ pages/api/judges.js
@@ -0,0 +1,8 @@
+//define an api route that our front-end will consume
+// import "../../styles/globals.css"
+import prisma from "../../lib/prisma";
+
+export default async function handle(req, res) {
+  const judges = await prisma.judges.findMany();
+  res.json(judges);
+}
