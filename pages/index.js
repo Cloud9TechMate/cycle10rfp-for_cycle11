8290a18a3733f220b9149ec80a0fef207a625feb
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-06-14 16:13:59 -0500

    Changed Name
    Originally the project was called prisma. It was a database tutorial. Changed the name to "cycle10rfp". We transferred components from our original project (MilwaukeeCountyKiosk) to this prisma  project to connect our database from Azure.
    
    Co-Authored-By: joyfortheppl <96784753+joyfortheppl@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 // import "../styles/globals.css"//can't import this
-import "../styles/Home.module.css"
+// import "../styles/Home.module.css"
 import "../styles/myCss.module.css"//this has to have .module.css
+import Card from "../pages/card";
+
 
 export const getServerSideProps = async() => {
     const res = await fetch("http://localhost:3000/api/judges");
@@ -15,6 +17,7 @@ export default function Judges({judges}) {
         <div>
             {judges.map((p) =>{
                 return (
+                    
                     <div key = {p.JUDGE} style ={{display: "flex"}}>
                         <p className = "para">{p.JUDGE}</p>
                         <p style = {{color: "blue"}}>{p.DEPUTY_COURT_CLERK}</p>
@@ -24,6 +27,39 @@ export default function Judges({judges}) {
                     </div>
                 )
             })}
+            <div>
+            return (
+    <div className="container">
+      <div className="row">
+        <Card
+          title="Courthouse Offices"
+          images="../pages/public/images/courthouse.png"
+          alt="courthouse"
+          
+        />
+        <Card
+        //"Job Opportunities"
+          title={<a href="https://county.milwaukee.gov/EN/Human-Resources">Job Opportunities</a>}
+          images="../images/Job-Free-PNG.png"
+          alt="Jobs"
+          // link = "https://county.milwaukee.gov/EN/Human-Resources"
+        />
+        <Card
+          title={<a href="https://county.milwaukee.gov/EN/News--Events/Events">Events</a>}
+          images="../images/EVENTS.png"
+          alt="events"
+
+        />
+        <Card
+          title="County Services"
+          images="../images/MilwaukeeCountyLogo.png"
+          alt="county"
+        
+        />
+      </div>
+    </div>
+            )
+            </div>
         </div>
     )
 }
